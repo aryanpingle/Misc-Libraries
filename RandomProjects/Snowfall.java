2b6df38d7c9770910af0fc266758657b51311eb4
robo-mop <49340972+robo-mop@users.noreply.github.com>
2019-11-02 22:13:15 +0530

    Updated the file

--- RandomProjects/Snowfall.java
+++ RandomProjects/Snowfall.java
@@ -13,11 +13,6 @@ public class Snowfall extends JPanel implements MouseListener, MouseWheelListene
 {
     static class Snow
     {
-        static final int maxRadius = 7;
-        static final int minRadius = 3;
-
-        static final double defaultVelocity = 2.3;
-
         Color color = new Color(255, 255, 255);
 
         Vector pos;
@@ -27,6 +22,9 @@ public class Snowfall extends JPanel implements MouseListener, MouseWheelListene
         double radius;
 
         double angle = 0;
+        double angleMove = 0.2;
+
+        int flakeType = 0;
         
         public Snow(double x, double y, double z)
         {
@@ -38,30 +36,48 @@ public class Snowfall extends JPanel implements MouseListener, MouseWheelListene
             */
             
             radius = p.map(z, -50, 50, maxRadius, minRadius);
-            vel = new Vector();
-            g = new Vector(0, p.map(z, -50, 50, 1, 0.1));
+            vel = new Vector(0, defaultVelocity);
+            g = new Vector(0, p.map(z, -50, 50, maxG, minG));
+            
+            color = C.getColorTowards(Color.WHITE, Color.BLACK, p.map(z, -50, 50, 0, 0.4));
+            
+            angle = p.map(Math.random(), 0, 1, 0, Math.PI*2);
+            angleMove = p.map(z, -50, 50, 0.1, 0.2);
+
+            flakeType = (int)p.map(Math.random(), 0, 1, 0, flakeTypes.length);
+            flakeType = (int)G.constrain(flakeType, 0, flakeTypes.length-1);
+        }
+
+        public void convertToNewFlake()
+        {
+            double z = p.map(Math.random(), 0, 1, -50, 50);
+            pos = new Vector(p.map(Math.random(), 0, 1, -fw/2, 3*fw/2), -50);
+            radius = p.map(z, -50, 50, maxRadius, minRadius);
+            vel = new Vector(0, defaultVelocity);
+            g = new Vector(0, p.map(z, -50, 50, maxG, minG));
+            color = C.getColorTowards(Color.WHITE, Color.BLACK, p.map(z, -50, 50, 0, 0.4));
+
+            flakeType = (int)p.map(Math.random(), 0, 1, 0, flakeTypes.length);
+            flakeType = (int)G.constrain(flakeType, 0, flakeTypes.length-1);
+            
+            angle = p.map(Math.random(), 0, 1, 0, Math.PI*2);
+            angleMove = p.map(z, -50, 50, 0.1, 0.2);
         }
         
         public void move()
         {
             pos.add(vel);
             vel.add(g);
-            g.add(new Vector(p.map(Math.sin(angle), -1, 1, -0.005, 0.005), 0));
+            vel.add(new Vector(p.map(Math.sin(angle), -1, 1, -0.005, 0.005), 0));
 
-            g.add(new Vector(p.map(slider.getValue(), slider.lb, slider.ub, -0.04, 0.04), 0));
+            vel.add(wind);
 
-            if(pos.j > fh || pos.i < -fw/2 || pos.i > 3*fw/2)
+            if(pos.j > fh || pos.i < -50 || pos.i > fw+50)
             {
-                double z = p.map(Math.random(), 0, 1, -50, 50);
-                pos = new Vector(p.map(Math.random(), 0, 1, -fw/2, 3*fw/2), -10);
-                radius = p.map(z, -50, 50, 10, 5);
-                vel = new Vector(0, defaultVelocity);
-                g = new Vector(0, p.map(z, -50, 50, 1, 0.1));
-                angle = p.map(Math.random(), 0, 1, 0, Math.PI*2);
-                color = C.getColorTowards(Color.WHITE, Color.BLACK, p.map(z, -50, 50, 0, 0.4));
+                convertToNewFlake();
             }
 
-            angle += 0.2;
+            angle += angleMove/10;
         }
         
         public void draw(Graphics g)
@@ -69,29 +85,44 @@ public class Snowfall extends JPanel implements MouseListener, MouseWheelListene
             double x = pos.i;
             double y = pos.j;
             
-            g.setColor(color);
-            g.fillOval((int)(x-radius), (int)(y-radius), (int)(2*radius), (int)(2*radius));
-            // g.drawImage(flake, (int)(x-radius), (int)(y-radius), (int)(2*radius), (int)(2*radius), null);
+            // g.setColor(color);
+            // g.fillOval((int)(x-radius), (int)(y-radius), (int)(2*radius), (int)(2*radius));
+            Graphics2D g2d = (Graphics2D)g.create();
+            g2d.rotate(angle/2, x, y);
+            g2d.drawImage(flakes[flakeType], (int)(x-radius), (int)(y-radius), (int)(2*radius), (int)(2*radius), null);
         }
     }
     
     static int T_DELAY = 35;
     static javax.swing.Timer timer;
     
-    static int fw = 750; // Width of Frame
-    static int fh = 750; // Height of Frame
+    static int fw = (int)p.ScreenWidth;//1500; // Width of Frame
+    static int fh = (int)p.ScreenHeight-90;//750; // Height of Frame
 
-    static int gw = 3*fw/2;
+    static int gw = fw;
     static int gh = fh;
     
-    static Color defaultBKG = new Color(50, 50, 50);
+    static Color defaultBKG = new Color(30, 30, 60);
     static Color bkg = defaultBKG;
     
     static LinkedList<Snow> list = new LinkedList<Snow>();
 
-    //static Image flake = new ImageIcon("images/Snowflake.png").getImage();
+    static Image flake = new ImageIcon("images/Flake.png").getImage();
+
+    static String[] flakeTypes = new String[]{"Flake2.png", "Flake3.png", "Flake4.png", "Flake5.png"};
+    static Image[] flakes;
 
     static Slider slider;
+
+    static Vector wind;
+
+    static final int maxRadius = 27;
+    static final int minRadius = 5;
+
+    static final double defaultVelocity = 2.5;
+
+    static final double maxG = 0.05;
+    static final double minG = 0.001;
     
     public static void main(String[] args)
     {
@@ -104,7 +135,7 @@ public class Snowfall extends JPanel implements MouseListener, MouseWheelListene
         
         JFrame f = new JFrame("New Project");
         
-        f.setSize(fw, fh+20);
+        f.setSize(fw, fh+10);
         f.setLocationRelativeTo(null);
         f.setUndecorated(true);
         
@@ -117,9 +148,17 @@ public class Snowfall extends JPanel implements MouseListener, MouseWheelListene
     {
         slider = new Slider(fw/2, fh, fw, 0, fw);
 
-        for(int i = 0; i < 300; i++)
+        flakes = new Image[flakeTypes.length];
+        for(int i = 0; i < flakeTypes.length; i++)
         {
-            list.add(new Snow(p.map(Math.random(), 0, 1, 0, fw), -10, p.map(Math.random(), 0, 1, -50, 50)));
+            flakes[i] = new ImageIcon("images/"+flakeTypes[i]).getImage();
+        }
+
+        wind = new Vector(p.map(slider.getValue(), slider.lb, slider.ub, -0.003, 0.003), 0);
+
+        for(int i = 0; i < 275; i++)
+        {
+            list.add(new Snow(p.map(Math.random(), 0, 1, 0, fw), p.map(Math.random(), 0, 1, -10, fh), p.map(Math.random(), 0, 1, -50, 50)));
         }
     }
     
@@ -148,12 +187,28 @@ public class Snowfall extends JPanel implements MouseListener, MouseWheelListene
             snow.draw(g);
         }
 
+        // double yo = fh-30;
+
+        // double tx = fw/2+yo*wind.i/((maxG+minG)/2);
+        // int size = 5;
+        // g.setColor(Color.WHITE);
+        // g.fillRect((int)(tx), (int)(yo), size, size);
+        // int n = 10;
+        // for(int i = 1; i <= n; i++)
+        // {
+        //     g.setColor(C.getColorTowards(Color.WHITE, bkg, (i-1)*1.0/n));
+        //     g.fillRect((int)(tx-i*size), (int)(yo), size, size);
+        //     g.fillRect((int)(tx+i*size), (int)(yo), size, size);
+        // }
+
         slider.draw(g);
     }
     
     @Override
     public void actionPerformed(ActionEvent e)
     {
+        wind = new Vector(p.map(slider.getValue(), slider.lb, slider.ub, -0.06, 0.06), 0);
+
         for(Snow snow: list)
         {
             snow.move();
