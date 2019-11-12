27f2bef4c59576d8cc17f8e0c63bc67eb756a5e2
robo-mop <49340972+robo-mop@users.noreply.github.com>
2019-11-12 10:51:59 +0530

    Refactored Code and Updated Vector.java

--- Vector.java
+++ Vector.java
@@ -1,195 +1,219 @@
-package PhysicsSim;
-
 import java.awt.*;
-import javax.swing.*;
 
 public class Vector
 {
+    static double pi = Math.PI;
     double i, j;
     // j points down for positive values
-
+    
+    public static void main(String[] args)
+    {
+        System.out.println(new Vector(-1, 1).rotate(-pi/2));
+    }
+    
     public Vector()
     {
         i = 0;
         j = 0;
     }
-
+    
     public Vector(double i, double j)
     {
         this.i = i;
         this.j = j;
     }
     
+    /**
+    * Creates a new vector object that points from (x1, y1) to (x2, y2)
+    */
+    
     public Vector(double x1, double y1, double x2, double y2)
     {
         this.i = x2-x1;
         this.j = y2-y1;
     }
-
+    
+    /**
+    * Creates a new vector object with direction ratios `i` and `j` with a net magnitude of `magnitude`
+    */
+    
     public Vector(double i, double j, double magnitude)
     {
         this.i = i;
         this.j = j;
-
+        
         setMagnitude(magnitude);
     }
-
+    
     /*
-     * Sets the i and j components of this vector to a random number in [0, factor]
-     */
-
+    * Sets the i and j components of this vector to a random number in [-factor, factor]
+    */
+    
     Vector randomize(double factor)
     {
         i = p.map(Math.random(), 0, 1, -factor, factor);
         j = p.map(Math.random(), 0, 1, -factor, factor);
-
+        
         return this;
     }
-
+    
+    /**
+    * Returns the angle of this vector from the POSITIVE X AXIS measured in the CLOCKWISE sense (in radians)
+    *
+    * A vector pointing towards (0, -1) returns 3*PI/2
+    * A vector pointing towards (-1, 1) returns 3*PI/4
+    * @return The correct, Euclidian angle of the vector considering positive j values to be pointing UP
+    */
+    
     public double getAngle()
     {
-        double mx = i;
-        double my = j;
-        int px = 0, py = 0;
-
-        if(mx == 0)
+        double y = j;
+        double x = i;
+        
+        if(y == 0 && x == 0)
         {
-            return (my>py?Math.PI/2:Math.PI*3/2);
+            return 0;
         }
-        if(my == 0)
+        
+        if(y == 0)
         {
-            return (mx<px?Math.PI:0);
+            return x>0?0:pi;
         }
-
-        if(mx > px && my > py)
+        if(x == 0)
         {
-            return Math.atan((my-py)/(mx-px));
+            return y>0?pi/2:3*pi/2;
         }
-        else if(mx > px && my < py)
+        if(y > 0 && x > 0) // Q1
         {
-            return (2*Math.PI+Math.atan((my-py)/(mx-px)));
+            return Math.atan(y/x);
         }
-        else if(mx < px && my > py)
+        else if(y > 0 && x < 0) // Q2
         {
-            return (Math.PI+Math.atan((my-py)/(mx-px)));
+            return pi+Math.atan(y/x);
         }
-        else
+        else if(y < 0 && x < 0) // Q3
         {
-            return (Math.PI+Math.atan((my-py)/(mx-px)));
+            return pi+Math.atan(y/x);
+        }
+        else // Q4
+        {
+            return 2*pi + Math.atan(y/x);
         }
     }
-
+    
     public double getMagnitude()
     {
         return Math.sqrt(i*i+j*j);
     }
-
-    /*
-     * Changes the state of the current vector object and returns its address
-     */
-
+    
+    /**
+    * Changes the state of the current vector object and returns its address
+    * @return The current vector object after adding `v` to it
+    */
+    
     public Vector add(Vector v)
     {
-        //return new Vector(i+v.i, j+v.j);
         i += v.i;
         j += v.j;
-
+        
         return this;
     }
-
-    /*
-     * Changes the state of the current vector object and returns its address
-     */
-
+    
+    /**
+    * Changes the state of the current vector object and returns its address
+    * @return The current vector object after subtracting `v` from it
+    */
+    
     public Vector sub(Vector v)
     {
-        //return new Vector(i-v.i, j-v.j);
         i -= v.i;
         j -= v.j;
-
+        
         return this;
     }
-
-    /*
-     * Changes the state of the current vector object and returns its address
-     */
-
+    
+    /**
+    * Changes the state of the current vector object and returns its address
+    * @return The current vector object after adding `v` to it
+    */
+    
     public Vector add(double i, double j)
     {
-        //return new Vector(this.i+i, this.j+j);
         this.i += i;
         this.j += j;
-
+        
         return this;
     }
-
-    /*
-     * Changes the state of the current vector object and returns its address
-     */
-
+    
+    /**
+    * Changes the state of the current vector object and returns its address
+    * @return The current vector object after subtracting `v` from it
+    */
+    
     public Vector sub(double i, double j)
     {
-        //return new Vector(this.i-i, this.j-j);
         this.i += i;
         this.j += j;
-
+        
         return this;
     }
-
+    
     public double dot(Vector v)
     {
         return i*v.i + j*v.j;
     }
-
+    
     public double dot(double i, double j)
     {
         return this.i*i + this.j*j;
     }
-
+    
     public Vector dot(double factor)
     {
         i *= factor;
         j *= factor;
-
+        
         return this;
     }
-
+    
     public Vector setMagnitude(double magnitude)
     {
+        if(i == 0 && j == 0)
+        {
+            return this;
+        }
+        
         double d = getMagnitude();
-
-        if(d == 0)
-        {
-            return this;
-        }
-        else
-        {
-            this.i = this.i*magnitude/d;
-            this.j = this.j*magnitude/d;
-
-            return this;
-        }
+        
+        this.i = this.i*magnitude/d;
+        this.j = this.j*magnitude/d;
+        
+        return this;
     }
-
+    
     public void draw(Graphics g, double ox, double oy, double factor)
     {
-        double angle = getAngle();
-
-        g.drawLine((int)(ox), (int)(oy), (int)(ox+factor*Math.cos(angle)), (int)(oy+factor*Math.sin(angle)));
+        Vector u = this.clone().normalize();
+        g.drawLine((int)(ox), (int)(oy), (int)(ox+factor*u.i), (int)(oy+factor*u.j));
     }
-
-    public Vector getUnitVector()
+    
+    /**
+    * Changes the current vector object to return its unit vector
+    */
+    
+    public Vector normalize()
     {
         this.i /= getMagnitude();
         this.j /= getMagnitude();
-
+        
         return this;
     }
-
-    /*
-     * Returns the component of the current vetor aboject along the vector `v`
-     */
-
+    
+    /**
+    * Returns the component of the current vetor object along the vector `v`
+    * @return A new vector object along the vector `v`
+    */
+    
     public Vector along(Vector v)
     {
         double dot = dot(v);
@@ -197,40 +221,41 @@ public class Vector
         Vector result = v.clone().dot(dot/(mag*mag));
         return result;
     }
-
-    /*
-     * Changes the state of the current vector object and returns its address
-     */
-
+    
+    /**
+    * Changes the state of the current vector object and returns its address
+    */
+    
     public Vector reverse()
     {
         i = -i;
         j = -j;
-
+        
         return this;
     }
-
-    /*
-     * Adds the angle in the COUNTER-CLOCKWISE sense
-     */
-
+    
+    /**
+    * Changes the current object by adding the angle in the COUNTER-CLOCKWISE sense
+    * @param angle The angle to be rotated by, in RADIANS
+    */
+    
     public Vector rotate(double angle)
     {
         angle += getAngle();
-
+        
         double mag = getMagnitude();
-
+        
         i = mag*Math.cos(angle);
         j = mag*Math.sin(angle);
-
+        
         return this;
     }
-
+    
     public Vector clone()
     {
         return new Vector(i, j);
     }
-
+    
     @Override
     public String toString()
     {
